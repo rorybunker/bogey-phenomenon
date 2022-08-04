95e37897fab908218d407d9852aad15b76862e2c
Rory Bunker <rorybunker@gmail.com>
2022-08-04 14:24:06 +0900

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# load virtual environment
+source ${HOME}/workspace2/virtualenvs/venv/bin/activate bogey
+# execute python scripts
+python -u /home/b_rory/workspace2/work/bogey/bogey_identification_tennis_v3.py
