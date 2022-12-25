885b8dd9ed08b906d7f9f35e2931b2342b9bdc6b
Rory Bunker <rorybunker@gmail.com>
2022-12-25 14:08:38 +0900

    Update create_data_clean_csv_files.R

--- create_data_clean_csv_files.R
+++ create_data_clean_csv_files.R
@@ -1,14 +1,22 @@
-# first, set your working directory to where your atp_2005_2020.RData and wta_2007_2020.RData files are downloaded to
-setwd("/Users/rorybunker/Google Drive/Research/Tennis Prediction ML/Data/Vincenzo Data/WElo supplementary material/")
+# --------------- R script to create the Data_Clean.csv file ---------------
+
+# set working directory
+# setwd("...")
+
 # install welo package if it is not already installed
 list.of.packages <- c("welo")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
+
 # load the welo library
 library("welo")
+
 # load the .RData file
-load("atp_2005_2020.RData")
+load("/RData/atp_2005_2020.RData")
+
 # apply the welo package's clean function to the loaded data
 db_clean <- clean(db)
+
 # output the cleaned dataset to a csv file
 write.csv(db_clean,"Data_Clean.csv", row.names = FALSE)
+# write.csv(db_clean,"Data_Clean_WTA.csv", row.names = FALSE)
